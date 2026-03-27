f725676ad0abf16bd88675b639aa6cb20f2440c2
shreynshah <shrey@geekcoder.net>
2026-03-26 23:08:24 -0700

    Update cockpit-ai to v0.1.3

--- Formula/cockpit-ai.rb
+++ Formula/cockpit-ai.rb
@@ -1,7 +1,7 @@
 class CockpitAi < Formula
   desc "Distributed personal agentic work orchestration system"
   homepage "https://github.com/shreynshah/cockpit.ai"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   depends_on "node@22"
@@ -9,7 +9,7 @@ class CockpitAi < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/shreynshah/cockpit.ai/releases/download/v#{version}/cockpit-#{version}-darwin-arm64.tar.gz"
-      sha256 "9114190024a26e2a868095d2b30f9b9f112d674c7b24ae47bde2f1f6afc76b4c" # darwin-arm64
+      sha256 "dd6b2ff4f51b2ffd9deb957a972ecb1ca1738cefa67b940de5d7a8fa69844c0d" # darwin-arm64
     else
       url "https://github.com/shreynshah/cockpit.ai/releases/download/v#{version}/cockpit-#{version}-darwin-x64.tar.gz"
       sha256 "" # darwin-x64
